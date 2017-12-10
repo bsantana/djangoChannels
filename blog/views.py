4ec2e9e8a9a8828c34a47365fa89ce65540dfee4
bsantana <bruno_shenn@hotmail.com>
2017-12-10 01:43:12 -0200

    salvando

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 # def post_list(request):
 #     return HttpResponse("Hello, worasasdadld. You're at the polls in2dex. NEW")
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
