2648c555ad3a190fe0067281d0490184f270ce22
jfelicio <git config --global user.email bruno_shenn@hotmail.comgit config --global user.email>
2017-12-11 14:56:39 -0200

    salvando

--- djangoChannels/settings.py
+++ djangoChannels/settings.py
@@ -130,4 +130,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = '/home/ubuntu/djangoChannels/static/'
 STATIC_URL = '/static/'
